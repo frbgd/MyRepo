8b583e00ab51399c8b23a96205ac6ba6b7703070
Алексей Кучерявенко <kap17597@gmail.com>
2018-11-18 02:50:41 +0300

    Winapithreads

--- thread4/thread4/thread4.cpp
+++ thread4/thread4/thread4.cpp
@@ -4,23 +4,24 @@
 
 using namespace std;
 
-void DoWork(int &a) {
+int Sum(int a, int b) {
 	this_thread::sleep_for(chrono::milliseconds(3000));
-	cout << "Thread ID = " << this_thread::get_id() << " =================\tDoWork started\t==================\n";
+	cout << "Thread ID = " << this_thread::get_id() << " =================\tSum started\t==================\n";
 	this_thread::sleep_for(chrono::milliseconds(5000));
-	a *= 5;
-	cout << "Thread ID = " << this_thread::get_id() << " =================\tDoWork stopped\t==================\n";
+	cout << "Thread ID = " << this_thread::get_id() << " =================\tSum stopped\t==================\n";
+	return a + b;
 }
 
 int main() {
-	int q = 5;
-	thread th(DoWork, std::ref(q));
+
+	int result;
+	thread t([&result]() {result = Sum(2, 5); });
 	for (size_t i = 0; i < 10; i++) {
 		cout << "Thread ID = " << this_thread::get_id() << "\tmain\t" << i << endl;
 		this_thread::sleep_for(chrono::milliseconds(500));
 	}
-	th.join();
-	cout << q << endl;
+	t.join();
+	cout << "Sum result: " << result << endl;
 	system("pause");
 	return 0;
-}
+} 
